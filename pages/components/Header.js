79d9fc0c88037705763ab70ae332f54b2bee3c0f
leliso <lelisoagegnehu491@gmail.com>
2022-09-23 11:32:46 +0300

    header done

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from '../../styles/Home.module.css'
-import Image from '../../images/avator.png'
+import Image from '../../images/avator.png';
 function Header() {
   return (
     <div className={styles.headercontainer}>
@@ -13,7 +13,7 @@ function Header() {
         </div>
       </div>
       <div className={styles.profile}>
-        <img src = {Image} alt ='profile' className={styles.image} />
+        <img src = 'https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png' alt ='profile' className={styles.image}/>
       </div>
     </div>
   )
