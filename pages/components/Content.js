d75464e85dda8deb0d791cefac8d0c868929c82a
leliso <lelisoagegnehu491@gmail.com>
2022-09-23 11:56:25 +0300

    content done

--- pages/components/Content.js
+++ pages/components/Content.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import styles from '../../styles/Home.module.css'
 
 function Content() {
   return (
-    <div>Content</div>
+    <div className={styles.contentcontainer}>
+      <div className={styles.contentwrapper}>
+        <div className={styles.tabs}>
+          <div className={styles.categories}>
+            <div>Companies</div>
+          </div>
+        </div>
+        <div className={styles.tabs}>
+          <div className={styles.categories}>
+            <div>Customer</div>
+          </div>
+        </div>
+        <div className={styles.tabs}>
+          <div className={styles.categories}>
+            <div>User</div>
+          </div>
+        </div>
+        <div className={styles.tabs}>
+          <div className={styles.categories}>
+            <div>Project</div>
+          </div>
+        </div>
+        <div className={styles.tabs}>
+          <div className={styles.categories}>
+            <div>Companies</div>
+          </div>
+        </div>
+        <div className={styles.tabs}>
+          <div className={styles.categories}>
+            <div>Companies</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
