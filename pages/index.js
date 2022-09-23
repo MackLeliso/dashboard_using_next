9501d14e9010e13e21f01213e0398f7a60ff679c
leliso <lelisoagegnehu491@gmail.com>
2022-09-23 10:00:46 +0300

    left navbar done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import LefttNavBar from './components/LefttNavBar'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>dashboard</h1>
+      <LefttNavBar />
     </div>
   )
 }
